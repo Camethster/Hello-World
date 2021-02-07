85bb361e0f5868f92756123688f6520dae31cbf4
Camethster <camethster101@byui.edu>
2021-02-06 23:28:29 -0700

    Final Product

--- FirebaseDatabase/CustomerData.py
+++ FirebaseDatabase/CustomerData.py
@@ -8,7 +8,7 @@ import json
 from tabulate import tabulate
   
 def intialize():
-    # should get intialize the database
+    #Intialize the database
     cred = credentials.Certificate("key.json")
     firebase_admin.initialize_app(cred,
                                     {"databaseURL" : "https://customerdata-39ba5-default-rtdb.firebaseio.com",
@@ -17,9 +17,9 @@ def intialize():
     return
 
 def get_connected():
-    # should pull up previous app 
+    # Get previous app settings
     app = firebase_admin.get_app("right")
-    # Return client  
+    # Return database reference
     return db.reference("/",app)
 
 def action():
@@ -32,11 +32,12 @@ def action():
             print("2) Change Customer")
             print("3) Remove Customer")
             print("4) Find Customer")
-            print("5) Quit")
+            print("5) Display Customers")
+            print("6) Quit")
             choice = int(input())
         except:
             print("Use the numbers listed please.")
-        if(choice > 5):
+        if(choice > 6):
             print("Use the numbers listed please.")
             choice = 0
     return choice
@@ -65,13 +66,12 @@ def set_base(db):
 def add_customer(db,fname,lname,address,city,state,zip_code,phone_number,status):
     # Get last customer name
     
-    last_cust = db.order_by_key().limit_to_last(1).get()
-    last_key = last_cust['Customer'].keys()
+    last_cust = db.child("Customer").order_by_key().limit_to_last(1).get()
+    last_key = last_cust.keys()
     s = "%s" % last_key
     new_index = "customer" + str(int(s.split("customer")[1].split("'")[0])+1)
-    
-    #map_values
-    db.child("Customer").set({
+    #Adds a new customer
+    db.child("Customer").update({
         new_index : 
         {
             "First_Name"  :  fname ,
@@ -85,12 +85,50 @@ def add_customer(db,fname,lname,address,city,state,zip_code,phone_number,status)
         }
     })
 
-def update_customer(db,customer):
-    find_customer(db,name)
+def update_customer(db,name):
+    #finds the customer and returns parent and child in tuple
+    customer = find_customer(db,name)
+    changing = True
+    while changing == True:
+        change = str.replace(input("What would you like to change?: ")," ","_")
+        avalue = input("To what value?")
+        db.child("Customer").child(customer[0]).update(
+            {
+                change : avalue
+            }
+        )
+
+        if input("Anymore changes on this Customer(y/n)? ") ==  "Y": 
+            changing = True 
+        else: 
+            changing = False
+
+def delete_customer(db,name):
+    customer = find_customer(db,name)
+    db.child("Customer").child(customer[0]).delete()
 
 def find_customer(db,name):
-    customer = db.child("Customer").equal_to(name).get()
-    print() 
+    #Finds customer using their first name
+    customer = db.child("Customer").get().items()
+    for child in customer:
+        values = child[1]
+        if values["First_Name"] == name:
+            print("First Name: %(First_Name)s" %values)
+            print("Last Name: %(Last_Name)s" %values)
+            print("Street Address: %(Street_Address)s" %values)
+            print("City: %(City)s" %values)
+            print("State: %(State)s" %values)
+            print("Zip Code: %(Zip_Code)s" %values)
+            print("Phone Number: %(Phone_Number)s" %values)
+            print("Status: %(Status)s" %values)
+            return child
+    print("Customer not found.")
+    return
+
+
+def display_customers(db):
+    customer = db.child("Customer").get().values()
+    print(tabulate(customer))
 
 
 def main():
@@ -99,13 +137,14 @@ def main():
         intialize()
         db = get_connected()
         set_base(db)
+        
     except ValueError:
         pass
 
     #establish connection
     db = get_connected()
-    
 
+    
     #UI
     done = False
     while not done:
@@ -128,10 +167,17 @@ def main():
             status =  input("Status? ") 
             add_customer(db,fname,lname,address,city,state,zip_code,phone_number,status)
         elif(choice == 2):
-        
+            name = input("Which customer do you want to update? ")
+            update_customer(db, name)
+        elif(choice == 3):
+            name = input("Which customer do you want to delete? ")
+            delete_customer(db,name)
         elif(choice == 4):
-            
-        elif choice == 5:
+            name = input("Which customer do you want to find? ")
+            find_customer(db, name)
+        elif(choice == 5):
+            display_customers(db)    
+        elif choice == 6:
             done = True 
 
 
